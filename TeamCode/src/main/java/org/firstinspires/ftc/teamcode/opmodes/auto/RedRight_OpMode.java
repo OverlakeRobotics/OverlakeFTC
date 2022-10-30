49f36268011961f62aaa8eb98302ea60cbdb4ea9
abansal <abansal@overlake.org>
2022-10-30 15:47:25 -0700

    Add subclasses to autonomous OpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/RedRight_OpMode.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.opmodes.auto;
+
+import static org.firstinspires.ftc.teamcode.opmodes.auto.CompetitionAutonomous.TeamState.RED_RIGHT;
+
+public class RedRight_OpMode extends CompetitionAutonomous {
+
+    public void init() {
+        super.init(RED_RIGHT);
+    }
+}
