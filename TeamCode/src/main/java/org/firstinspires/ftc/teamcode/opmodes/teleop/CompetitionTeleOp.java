07561bde1522bf6051e45f8ccda6b7ae8431e678
anjikaarora <aarora@overlake.org>
2022-11-16 16:03:16 -0800

    i changed the value of how fast the gamepad right stuck x moves and also added the ramp code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/CompetitionTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/CompetitionTeleOp.java
@@ -17,7 +17,7 @@ public class CompetitionTeleOp extends BaseOpMode {
      * Left trigger engages slow-drive
      */
     public void loop() {
-        float rx = (float) Math.pow(gamepad1.right_stick_x, 3);
+        float rx = (float) Math.pow(gamepad1.right_stick_x, 3.5);
         float lx = (float) Math.pow(gamepad1.left_stick_x, 3);
         float ly = (float) Math.pow(gamepad1.left_stick_y, 3);
         driveSystem.slowDrive(gamepad1.left_trigger > 0.3f);
